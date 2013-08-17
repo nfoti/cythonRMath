392a27f7ce235370846cc45d83498ccdfa64bc88
Nick Foti <nfoti01@gmail.com>
2013-08-17 14:38:47 -0700

    Made actual setup.py.

--- setup.py
+++ setup.py
@@ -1,2 +1,25 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from Cython.Distutils import Extension
+from Cython.Distutils import build_ext
 
-# Placeholder setup.py
+setup(
+    name="cythonRmath",
+    version="0.0.1",
+    author="Nicholas J. Foti",
+    author_email="nfoti01@gmail.com",
+    url="http://github.com/nfoti/cythonRmath",
+    packages=["cython_rmath"],
+    package_data={"cython_rmath": ["*.pxd"]},
+    description="""Cython declarations for the Rmath Library.""",
+    setup_requires=['Cython >= 0.16'],
+    install_requires=['Cython >= 0.16'],
+    classifiers=[
+                'Development Status :: 4 - Beta',
+                'Environment :: Console',
+                'Operating System :: OS Independent',
+                'Intended Audience :: Science/Research',
+                'License :: OSI Approved :: GNU General Public License (GPL)',
+                'Programming Language :: Python',
+                'Topic :: Scientific/Engineering']
+)
